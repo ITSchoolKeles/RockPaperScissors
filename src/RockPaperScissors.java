4e7d2b8e4f9e6c2a39189c31b9de0ef0b56c6165
ElyorAzimovJava <ayubjonyahiyoyev@gmail.com>
2025-12-09 16:26:50 +0500

    Multipletime gaming is implemented by using for loop and live and the final result displayment are added

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,10 +1,16 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.SortedMap;
 
 public class RockPaperScissors {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        /*
+        * O'yinni n marta oynaydigan qilamiz(for)
+        * Yakuniy natijani chiqaraydigan qilamiz --> 2 ta galaba 1 ta durang 1 mag'lubiyat
+        * */
         // randomSonlarni generatsiya qilib beradi
+
         Random random = new Random();
         final String ROCK = "ROCK";
         final String PAPER = "PAPER";
@@ -13,67 +19,104 @@ public class RockPaperScissors {
         final String WIN = "WIN";
         final String LOSE = "LOSE";
         final String INVALID_CHOICE_NUMBER = "Invalid Choice Number";
+        int victoryCount = 0;
+        int loseCount = 0;
+        int drawCount = 0;
 
-        // compyuterni random tanlashini qilish
-            // random son generatsiya qilish
-        int generatedRanNumber = random.nextInt(0, 3);
-        // random raqamni tanlovga moslash
-        String computerChoice = "";
-        switch (generatedRanNumber) {
-            case 0: computerChoice = ROCK; break;
-            case 1: computerChoice = PAPER; break;
-            case 2: computerChoice = SCISSORS; break;
-        }
+        System.out.print("Iltimos o'yin sonini kiriting ==> ");
+         int gameCount = input.nextInt();
 
-    // player choice
-        String playerChoice = "";
-        System.out.print("Enter the numbers in this range[0-2] ");
-        int playerChoiceNumber = input.nextInt();
-        playerChoice = switch (playerChoiceNumber){
-            case 0 ->  ROCK;
-            case 1 ->  PAPER;
-            case 2 ->  SCISSORS;
-            default -> INVALID_CHOICE_NUMBER;
-        };
+         for(int i = 0; i < gameCount; i++){
+             // compyuterni random tanlashini qilish
+             // random son generatsiya qilish
+             int generatedRanNumber = random.nextInt(0, 3);
+             // random raqamni tanlovga moslash
+             String computerChoice = "";
+             switch (generatedRanNumber) {
+                 case 0: computerChoice = ROCK; break;
+                 case 1: computerChoice = PAPER; break;
+                 case 2: computerChoice = SCISSORS; break;
+             }
 
-        if(playerChoice.equals(INVALID_CHOICE_NUMBER)){
-            System.out.println("You have picked an invalid choice.");
-            return;
-        }
+             // player choice
+             String playerChoice = "";
+             System.out.print("Enter the numbers in this range[0-2] ");
+             int playerChoiceNumber = input.nextInt();
+             playerChoice = switch (playerChoiceNumber){
+                 case 0 ->  ROCK;
+                 case 1 ->  PAPER;
+                 case 2 ->  SCISSORS;
+                 default -> INVALID_CHOICE_NUMBER;
+             };
 
-        String result = "";
-        if(playerChoice.equals(ROCK)){
-            if(computerChoice.equals(SCISSORS)){
-                result = WIN;
-            }else if(computerChoice.equals(PAPER)){
-                result = LOSE;
-            }else {
-                result = DRAW;
-            }
-        }else if(playerChoice.equals(SCISSORS)){
-            if(computerChoice.equals(PAPER)){
-                result = WIN;
-            }else if(computerChoice.equals(ROCK)){
-                result = LOSE;
-            }else {
-                result = DRAW;
-            }
-        }
-        // paper uchun natija qismini logikasini yozish
+             if(playerChoice.equals(INVALID_CHOICE_NUMBER)){
+                 System.out.println("You have picked an invalid choice.");
+                 return;
+             }
+
+             String result = "";
+             if(playerChoice.equals(ROCK)){
+                 if(computerChoice.equals(SCISSORS)){
+                     result = WIN;
+                     victoryCount++;
+                 }else if(computerChoice.equals(PAPER)){
+                     result = LOSE;
+                     loseCount++;
+                 }else {
+                     result = DRAW;
+                     drawCount++;
+                 }
+             }else if(playerChoice.equals(SCISSORS)){
+                 if(computerChoice.equals(PAPER)){
+                     result = WIN;
+                     victoryCount++;
+                 }else if(computerChoice.equals(ROCK)){
+                     result = LOSE;
+                     loseCount++;
+                 }else {
+                     result = DRAW;
+                     drawCount++;
+                 }
+             }else{
+                 if(computerChoice.equals(ROCK)){
+                     result = WIN;
+                     victoryCount++;
+                 }else if(computerChoice.equals(SCISSORS)){
+                     result = LOSE;
+                     loseCount++;
+                 }else {
+                     result = DRAW;
+                     drawCount++;
+                 }
+             }
+             // paper uchun natija qismini logikasini yozish
 
 
 
-        System.out.println("Computer choice is " + computerChoice);
-        System.out.println("Player choice is " + playerChoice);
-        System.out.println("Result is " + result);
-        // rock vs rock = draw; rock vs scissors = win; rock vs paper = lose;
+             System.out.println("Computer choice is " + computerChoice);
+             System.out.println("Player choice is " + playerChoice);
+             System.out.println("Result is " + result);
+             System.out.println("------------------  Live result table -----------------");
+             System.out.println("WC - " + victoryCount + " LC - " + loseCount + " DC - " + drawCount);
+             System.out.println("------------------------------------------------------");
+
+             // rock vs rock = draw; rock vs scissors = win; rock vs paper = lose;
+         }
+         if(victoryCount > loseCount){
+             System.out.println("CONGRATULATIONS🎉🎉🎉🎉. You win");
+         }else if(victoryCount == loseCount){
+             System.out.println("DRAWWWWWWWWWW");
+         }else{
+             System.out.println("OOPSSSSS! You lose😥😥😥😥😥");
+         }
 
 
 
-        //
-        // 0 --> rock
-        // 1 --> paper
-        // 2 --> scissors
+
+
+
+
+
 
 
     }
